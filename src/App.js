814bc61233d81ef5c66ae3946507619c97964579
franobaya <franciscoobaya99@gmail.com>
2022-08-10 22:18:38 -0300

    primercommit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my first React app
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/franobaya"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          More about Me
         </a>
       </header>
     </div>
