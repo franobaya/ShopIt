61c454ace26a3e86ce6041cfec9d8dc9e7baea09
franobaya <franciscoobaya99@gmail.com>
2022-08-17 17:27:32 -0300

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './NavBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my first React app
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/franobaya"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          More about Me
-        </a>
-      </header>
-    </div>
+    <NavBar />
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+
+const NavBar=()=>{
+    return(
+        <header>
+            <h3 className="Brand" alt="logo">ShopIt</h3>
+            <nav>
+                <ul className="navlinks">
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Sales</a></li>
+                </ul>
+            </nav>
+            <a className="cta" href="#"><button>Cart</button></a>
+        </header>
+        
+
+    )
+}
+export default NavBar;
